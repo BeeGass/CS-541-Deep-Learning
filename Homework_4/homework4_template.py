010dc4f63183d40258a337dd77128170fbd30941
Bryan <bryank123@live.com>
2021-02-24 15:03:35 -0500

    Added class stuff

---
+++ Homework_4/homework4_template.py
@@ -0,0 +1,160 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import scipy.optimize
+
+# For this assignment, assume that every hidden layer has the same number of neurons.
+NUM_HIDDEN_LAYERS = 3
+NUM_INPUT = 784
+NUM_HIDDEN = 10
+NUM_OUTPUT = 10
+
+# Unpack a list of weights and biases into their individual np.arrays.
+def unpack (weightsAndBiases):
+    # Unpack arguments
+    Ws = []
+
+    # Weight matrices
+    start = 0
+    end = NUM_INPUT*NUM_HIDDEN
+    W = weightsAndBiases[start:end]
+    Ws.append(W)
+
+    for i in range(NUM_HIDDEN_LAYERS - 1):
+        start = end
+        end = end + NUM_HIDDEN*NUM_HIDDEN
+        W = weightsAndBiases[start:end]
+        Ws.append(W)
+
+    start = end
+    end = end + NUM_HIDDEN*NUM_OUTPUT
+    W = weightsAndBiases[start:end]
+    Ws.append(W)
+
+    Ws[0] = Ws[0].reshape(NUM_INPUT, NUM_HIDDEN)
+    for i in range(1, NUM_HIDDEN_LAYERS):
+        # Convert from vectors into matrices
+        Ws[i] = Ws[i].reshape(NUM_HIDDEN, NUM_HIDDEN)
+    Ws[-1] = Ws[-1].reshape(NUM_HIDDEN, NUM_OUTPUT)
+
+    # Bias terms
+    bs = []
+    start = end
+    end = end + NUM_HIDDEN
+    b = weightsAndBiases[start:end]
+    bs.append(b)
+
+    for i in range(NUM_HIDDEN_LAYERS - 1):
+        start = end
+        end = end + NUM_HIDDEN
+        b = weightsAndBiases[start:end]
+        bs.append(b)
+
+    start = end
+    end = end + NUM_OUTPUT
+    b = weightsAndBiases[start:end]
+    bs.append(b)
+
+    return Ws, bs
+
+def forward_prop (x, y, weightsAndBiases):
+    Ws, bs = unpack(weightsAndBiases)
+
+    # Return loss, pre-activations, post-activations, and predictions
+    return loss, zs, hs, yhat
+   
+def back_prop (x, y, weightsAndBiases):
+    loss, zs, hs, yhat = forward_prop(x, y, weightsAndBiases)
+
+    dJdWs = []  # Gradients w.r.t. weights
+    dJdbs = []  # Gradients w.r.t. biases
+
+    # TODO
+    for i in range(NUM_HIDDEN_LAYERS, -1, -1):
+        pass
+        # TODO
+        
+    # Concatenate gradients
+    return np.hstack([ dJdW.flatten() for dJdW in dJdWs ] + [ dJdb.flatten() for dJdb in dJdbs ]) 
+
+def train (trainX, trainY, weightsAndBiases, testX, testY):
+    NUM_EPOCHS = 100
+    trajectory = []
+    for epoch in range(NUM_EPOCHS):
+        # TODO: implement SGD.
+        # TODO: save the current set of weights and biases into trajectory; this is
+        # useful for visualizing the SGD trajectory.
+        
+    return weightsAndBiases, trajectory
+
+# Performs a standard form of random initialization of weights and biases
+def initWeightsAndBiases ():
+    Ws = []
+    bs = []
+
+    np.random.seed(0)
+    W = 2*(np.random.random(size=(NUM_INPUT, NUM_HIDDEN))/NUM_INPUT**0.5) - 1./NUM_INPUT**0.5
+    Ws.append(W)
+    b = 0.01 * np.ones(NUM_HIDDEN)
+    bs.append(b)
+
+    for i in range(NUM_HIDDEN_LAYERS - 1):
+        W = 2*(np.random.random(size=(NUM_HIDDEN, NUM_HIDDEN))/NUM_HIDDEN**0.5) - 1./NUM_HIDDEN**0.5
+        Ws.append(W)
+        b = 0.01 * np.ones(NUM_HIDDEN)
+        bs.append(b)
+
+    W = 2*(np.random.random(size=(NUM_HIDDEN, NUM_OUTPUT))/NUM_HIDDEN**0.5) - 1./NUM_HIDDEN**0.5
+    Ws.append(W)
+    b = 0.01 * np.ones(NUM_OUTPUT)
+    bs.append(b)
+    return np.hstack([ W.flatten() for W in Ws ] + [ b.flatten() for b in bs ])
+
+def plotSGDPath (trainX, trainY, trajectory):
+    # TODO: change this toy plot to show a 2-d projection of the weight space
+    # along with the associated loss (cross-entropy), plus a superimposed 
+    # trajectory across the landscape that was traversed using SGD. Use
+    # sklearn.decomposition.PCA's fit_transform and inverse_transform methods.
+
+    def toyFunction (x1, x2):
+        return np.sin((2 * x1**2 - x2) / 10.)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    # Compute the CE loss on a grid of points (corresonding to different w).
+    axis1 = np.arange(-np.pi, +np.pi, 0.05)  # Just an example
+    axis2 = np.arange(-np.pi, +np.pi, 0.05)  # Just an example
+    Xaxis, Yaxis = np.meshgrid(axis1, axis2)
+    Zaxis = np.zeros((len(axis1), len(axis2)))
+    for i in range(len(axis1)):
+        for j in range(len(axis2)):
+            Zaxis[i,j] = toyFunction(Xaxis[i,j], Yaxis[i,j])
+    ax.plot_surface(Xaxis, Yaxis, Zaxis, alpha=0.6)  # Keep alpha < 1 so we can see the scatter plot too.
+
+    # Now superimpose a scatter plot showing the weights during SGD.
+    Xaxis = 2*np.pi*np.random.random(8) - np.pi  # Just an example
+    Yaxis = 2*np.pi*np.random.random(8) - np.pi  # Just an example
+    Zaxis = toyFunction(Xaxis, Yaxis)
+    ax.scatter(Xaxis, Yaxis, Zaxis, color='r')
+
+    plt.show()
+
+if __name__ == "__main__":
+    # TODO: Load data and split into train, validation, test sets
+    # trainX = ...
+    # trainY = ...
+    # ...
+
+    # Initialize weights and biases randomly
+    weightsAndBiases = initWeightsAndBiases()
+
+    # Perform gradient check on random training examples
+    print(scipy.optimize.check_grad(lambda wab: forward_prop(np.atleast_2d(trainX[0:5,:]), np.atleast_2d(trainY[0:5,:]), wab)[0], \
+                                    lambda wab: back_prop(np.atleast_2d(trainX[0:5,:]), np.atleast_2d(trainY[0:5,:]), wab), \
+                                    weightsAndBiases))
+
+    weightsAndBiases, trajectory = train(trainX, trainY, weightsAndBiases, testX, testY)
+    
+    # Plot the SGD trajectory
+    plotSGDPath(trainX, trainY, ws)
